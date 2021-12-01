588c3b0e3474b7331ea197023fd5f14a2c9528cd
Mihir Baidya <mihirbaidya08@gmail.com>
2021-12-02 02:44:24 +0530

    add footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import PreNavbar from "./components/PreNavbar";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Slider from "./components/Slider.js";
 import data from "./data/data.json";
 import Offers from "./components/Offers.js";
@@ -13,6 +13,7 @@ import ProductReviews from "./components/ProductReviews";
 import Videos from "./components/Videos";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
+import NavOptions from "./components/NavOptions";
 
 function App() {
     return (
@@ -20,13 +21,23 @@ function App() {
             <Router>
                 <PreNavbar />
                 <Navbar />
+                <NavOptions
+                    miPhones={data.miPhones}
+                    redmiPhones={data.redmiPhones}
+                    tv={data.tv}
+                    laptop={data.laptop}
+                    fitnessAndLifeStyle={data.fitnessAndLifeStyle}
+                    home={data.home}
+                    audio={data.audio}
+                    accessories={data.accessories}
+                />
                 <Slider start={data.banner.start} />
                 <Offers offer={data.offer} />
                 <Heading text="STAR PROJECTS" />
                 <StarProducts starProduct={data.starProduct} />
                 <Heading text="HOT ACCESSORIES" />
                 <HotAccessoriesMenu />
-                <Routes>
+                <Switch>
                     <Route
                         exact
                         path="/music"
@@ -85,14 +96,14 @@ function App() {
                             />
                         }
                     ></Route>
-                </Routes>
+                </Switch>
                 <Heading text="PRODUCT REVIEWS" />
                 <ProductReviews productReviews={data.productReviews} />
                 <Heading text="VIDEOS" />
                 <Videos videos={data.videos} />
                 <Heading text="IN THE PRESS" />
                 <Banner banner={data.banner} />
-                <Footer />
+                <Footer footer={data.footer} />
             </Router>
         </div>
     );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,120 @@
 import React from "react";
+import "../styles/PreFooter.css";
+import "../styles/Footer.css";
 
-const Footer = () => {
-    return <div>Hello from footer</div>;
+const Footer = ({ footer }) => {
+    return (
+        <>
+            <div className="PreFooter">
+                <div>
+                    <i className="fas fa-redo"></i>
+                    <p>
+                        <b>Hassle-free replacement</b>
+                        <br />
+                        10-day easy replacement policy on mi.com
+                    </p>
+                </div>
+                <div>
+                    <i className="fas fa-user-shield"></i>
+                    <p>
+                        <b>100% secure payments</b>
+                        <br />
+                        We support Cards, Wallets, EMI and COD
+                    </p>
+                </div>
+                <div>
+                    <i className="fas fa-map-marked-alt"></i>
+                    <p>
+                        <b>Vast service network</b>
+                        <br />
+                        1000 MI service-centers across 600 cities
+                    </p>
+                </div>
+            </div>
+            <div className="PreFooter2">
+                <div>
+                    <p>LET'S STAY IN TOUCH</p>
+                    <span>Get updates on sales specials and more</span>
+                </div>
+                <div>
+                    <div>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Enter Email Address"
+                        />
+                        <button>
+                            <i className="fas fa-chevron-right"></i>
+                        </button>
+                    </div>
+                    {/* <span>
+                        Thanks. You're on our email list for special offers.
+                    </span> */}
+                </div>
+                <div>
+                    <p>FOLLOW MI</p>
+                    <span>We want to hear from you!</span>
+                </div>
+                <div>
+                    <i className="fab fa-facebook-f"></i>
+                    <i className="fab fa-youtube"></i>
+                    <i className="fab fa-instagram"></i>
+                    <i className="fab fa-twitter"></i>
+                </div>
+            </div>
+            <div className="footer">
+                <div>
+                    <p>SUPPORT</p>
+                    {footer.support.map((item, index) => (
+                        <a key={item.url} href={item.url}>
+                            {item.name}
+                        </a>
+                    ))}
+                </div>
+                <div>
+                    <p>SHOP AND LEARN</p>
+                    {footer.shopAndLearn.map((item, index) => (
+                        <a href={item.url} key={item.url}>
+                            {item.name}
+                        </a>
+                    ))}
+                </div>
+                <div>
+                    <p>RETAIL STORE</p>
+                    {footer.retailStore.map((item, index) => (
+                        <a href={item.url} key={item.url}>
+                            {item.name}
+                        </a>
+                    ))}
+                </div>
+                <div>
+                    <p>ABOUT</p>
+                    {footer.aboutUS.map((item, index) => (
+                        <a href={item.url} key={item.url}>
+                            {item.name}
+                        </a>
+                    ))}
+                </div>
+                <div>
+                    <p>CONTACT US</p>
+                    {footer.contactUs.map((item, index) => (
+                        <a href={item.url} key={item.url}>
+                            {item.name}
+                        </a>
+                    ))}
+                </div>
+                <div>
+                    <div>
+                        Chat with our Virtual AI Bot (24/7 Live Agent Support)
+                    </div>
+                    <button>Chat Now</button>
+                </div>
+            </div>
+            <div className="footerBorder">
+                <div>Copyright © 2010 - 2022 Xiaomi. All Rights Reserved</div>
+            </div>
+        </>
+    );
 };
 
 export default Footer;
